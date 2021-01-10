4a42087ffcd8190fa6fd8e0a185a248a7c4342f8
Adolfo Unutrbe <apu314@apu314.com>
2021-01-10 21:31:20 +0000

    add https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
     const apiKey = 'plBzcPxA0YD6D5UTV90QJnBKzaZmVla9';
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ encodeURI(category) }&lang=es&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ encodeURI(category) }&lang=es&limit=10`;
 
     const resp = await fetch(url);
     const { data } = await resp.json();
